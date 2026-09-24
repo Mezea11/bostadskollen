8f8f390fe7d8628d1d5026dc3247f8f009e95c4a
lakevalley <lakevalleymalmo@gmail.com>
2026-09-24 21:03:27 +0200

    Changed app description

--- app.py
+++ app.py
@@ -399,7 +399,7 @@ with st.sidebar:
     st.title("🏠 Boprisindikatorn")
 
     st.write(
-        "Uppskatta en bostads utgångspris med en modell tränad i notebooken."
+        "Gör en prisuppskattning med en tränad maskininlärningsmodell."
     )
 
     st.divider()
